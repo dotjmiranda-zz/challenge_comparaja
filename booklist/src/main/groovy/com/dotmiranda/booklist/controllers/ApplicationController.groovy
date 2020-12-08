e80bc88eb469bfaf652f9e36a6bb1aa7ce3b7c9f
João Miranda <dot.jmiranda@gmail.com>
2020-12-08 17:24:03 +0000

    config system varibles

--- booklist/src/main/groovy/com/dotmiranda/booklist/controllers/ApplicationController.groovy
+++ booklist/src/main/groovy/com/dotmiranda/booklist/controllers/ApplicationController.groovy
@@ -1,4 +1,12 @@
 package com.dotmiranda.booklist.controllers
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class ApplicationController {
+    @GetMapping("/")
+    String Home() {
+        return "Root"
+    }
 }
